50eb1336ab8ce0a02df40ebe374d50163cf35a51
keesvanz <kvanderaa@gmail.com>
2017-03-09 16:12:24 -0600

    [Almost Done. Got all three rolls to work and record]

--- dice.py
+++ dice.py
@@ -1,8 +1,94 @@
-print ("1v1 dice game")
-print ("best out of 3")
+print("What is player 1 name?")
+player1name = raw_input("Name:")
+print("what is player 2 name?")
+player2name = raw_input("Name:")
 
-print ("player one goes first")
+print("time to play")
 
-raw_input("type in roll")
+print("Player 1 roll dice")
+raw_input("roll dice? press enter.")
+
 import random
-print (random.randint(1,6))
+Roll1 = (random.randint(1,6))
+print(Roll1)
+
+print("Player 2 roll dice")
+raw_input("roll dice? Press enter.")
+
+import random
+Roll2 = (random.randint(1,6))
+print(Roll2)
+
+if Roll1 > Roll2:
+	print(player1name+" wins!")
+elif Roll1 == Roll2:
+	print("tie")
+if Roll2 > Roll1:
+	print(player2name+" wins!")
+
+
+
+
+print("Round 2!")
+
+
+
+
+
+
+
+
+print("Player 1 roll dice")
+raw_input("roll dice? press enter.")
+
+import random
+Roll1 = (random.randint(1,6))
+print(Roll1)
+
+print("Player 2 roll dice")
+raw_input("roll dice? Press enter.")
+
+import random
+Roll2 = (random.randint(1,6))
+print(Roll2)
+
+if Roll1 > Roll2:
+	print(player1name+" wins!")
+elif Roll1 == Roll2:
+	print("tie")
+if Roll2 > Roll1:
+	print(player2name+" wins!")
+
+print("Round 3!")
+
+
+
+
+
+
+
+
+print("Player 1 roll dice")
+raw_input("roll dice? press enter.")
+
+import random
+Roll1 = (random.randint(1,6))
+print(Roll1)
+
+print("Player 2 roll dice")
+raw_input("roll dice? Press enter.")
+
+import random
+Roll2 = (random.randint(1,6))
+print(Roll2)
+
+if Roll1 > Roll2:
+	print(player1name+" wins!")
+elif Roll1 == Roll2:
+	print("tie")
+if Roll2 > Roll1:
+	print(player2name+" wins!")
+
+
+
+
